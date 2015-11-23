abe4248902dfc273583b3d90e8b9829e57b08d17
cxm_lmz@163.com <cxm15936248237>
2015-11-23 22:30:43 +0800

    add the futus url

--- app/src/main/java/com/terry/MainActivity.java
+++ app/src/main/java/com/terry/MainActivity.java
@@ -1,13 +1,73 @@
 package com.terry;
 
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.widget.TextView;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            String doc = msg.getData().getString("doc");
+            textview.setText(doc);
+        }
+    };
+
+    private TextView textview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textview = (TextView) findViewById(R.id.textview);
+        textview.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new AsyTask().execute();
+    }
+
+    class AsyTask extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textview.setText(s);
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            Document document = null;
+            try {
+                document = Jsoup.connect("http://www.qbaobei.com/jiaoyu/tj/tjgs/List_2.html").timeout(5000)
+                            .post();
+                Elements elements = document.getElementsByClass("div.tips");
+                Element element = elements.first();
+                Log.v("cxm", element.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return document.toString();
+        }
+    }
+
 }
